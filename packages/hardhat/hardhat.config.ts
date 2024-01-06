713a53d4511341b5485890dc03322263d2f29ac8
Paul Burke <paul@paulburke.co>
2024-01-05 20:28:02 -0500

    fix: fixes solc download issue; upgrades to solidity 0.8.21

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -22,7 +22,7 @@ const etherscanApiKey = process.env.ETHERSCAN_API_KEY || "DNXJA8RX2Q3VZ4URQIWP7Z
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.19",
+    version: "0.8.21",
     settings: {
       optimizer: {
         enabled: true,
