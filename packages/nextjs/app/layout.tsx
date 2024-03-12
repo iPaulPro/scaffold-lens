af6627c3f5587d857e6facab8a0f727796324f52
Paul Burke <paul@paulburke.co>
2024-03-12 16:30:17 -0400

    chore: updates nextjs page metadata

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -9,9 +9,9 @@ const baseUrl = process.env.VERCEL_URL
   : `http://localhost:${process.env.PORT || 3000}`;
 const imageUrl = `${baseUrl}/thumbnail.jpg`;
 
-const title = "Scaffold-ETH 2 App";
-const titleTemplate = "%s | Scaffold-ETH 2";
-const description = "Built with 🏗 Scaffold-ETH 2";
+const title = "Scaffold-Lens App";
+const titleTemplate = "%s | Scaffold-Lens";
+const description = "Built with Scaffold-Lens, a fork of 🏗 Scaffold-ETH 2";
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
