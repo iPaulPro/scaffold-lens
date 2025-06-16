00d5a3fb3e4e61206f0e9bcb8d2fbb1107c21027
Paul Burke <paul@paulburke.co>
2025-06-15 22:29:13 -0400

    chore: updates lens chain definitions

--- packages/nextjs/utils/lensChains.ts
+++ packages/nextjs/utils/lensChains.ts
@@ -6,7 +6,7 @@ export const inMemoryNode = defineChain({
   ...chains.zksyncInMemoryNode,
   contracts: {
     multicall3: {
-      address: "0x07De5CBeaB32e6b86ae6b993A62B4a1759c66217",
+      address: "0x07De5CBeaB32e6b86ae6b993A62B4a1759c66217", // replace with local deployed address
     },
   },
 });
@@ -22,19 +22,22 @@ export const lensTestnet = defineChain({
   },
   rpcUrls: {
     default: {
-      http: ["https://rpc.testnet.lens.dev"],
-      webSocket: ["wss://rpc.testnet.lens.dev/ws"],
+      http: ["https://rpc.testnet.lens.xyz"],
+      webSocket: ["wss://rpc.testnet.lens.xyz/ws"],
     },
   },
   blockExplorers: {
     default: {
       name: "Lens Testnet Block Explorer",
-      url: "https://block-explorer.testnet.lens.dev/",
+      url: "https://explorer.testnet.lens.xyz/",
     },
   },
   contracts: {
     multicall3: {
       address: "0x8A44EDE8a6843a997bC0Cc4659e4dB1Da8f91116",
+    },
+    wrappedGasToken: {
+      address: "0xeee5a340Cdc9c179Db25dea45AcfD5FE8d4d3eB8",
     },
   },
   testnet: true,
@@ -64,7 +67,9 @@ export const lens = defineChain({
   contracts: {
     multicall3: {
       address: "0x6b6dEa4D80e3077D076733A04c48F63c3BA49320",
-      wrappedGasToken: "0x6bdc36e20d267ff0dd6097799f82e78907105e2f",
+    },
+    wrappedGasToken: {
+      address: "0x6bDc36E20D267Ff0dd6097799f82e78907105e2F",
     },
   },
   testnet: false,
